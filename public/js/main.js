7001fa100ab2008b2ed537e55e8c36b12a3f2e18
tinho <jhonbest170@gmail.com>
2024-02-05 20:10:01 -0300

    Projeto original da @CodeSagaAcademy

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+window.animation = ScrollReveal({reset:true})
+
+animation.reveal('.txt',{
+  duration:800,
+  delay:200,
+  distance:"90px",
+  origin:"left"
+})
+animation.reveal('.text-h1',{
+  duration:800,
+  delay:200,
+  distance:"90px",
+  origin:"left"
+})
+animation.reveal('.f',{
+  duration:800,
+  delay:200,
+  distance:"90px",
+  origin:"left"
+})
+animation.reveal('.p', {
+  duration: 800,
+  delay:300,
+  distance: "90px",
+  origin: "bottom"
+})
+
